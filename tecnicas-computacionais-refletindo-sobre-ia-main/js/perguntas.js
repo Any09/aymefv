45803b2d7eda3e11f9bd71e5904951db186cb654
Anny <104777994+Any09@users.noreply.github.com>
2024-07-25 15:23:19 -0300

    hfgthyhyfg

--- tecnicas-computacionais-refletindo-sobre-ia-main/js/perguntas.js
+++ tecnicas-computacionais-refletindo-sobre-ia-main/js/perguntas.js
@@ -1,12 +1,12 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Agora você vai enfrentar o larbirinto do Minotauro, uma história famosa da cultura grega. Na Grécia Antiga, diz a lenda, existiu uma criatura feroz, com cabeça de touro e corpo de homem, aprisionada no labirinto de Creta. O minotauro, era filho da rainha Parsífae, esposa de Minos, rei de Creta, com o touro branco, pelo qual ela se apaixonou como castigo de Netuno, deus do mar. Para aplacar a fome do monstro, o rei Minos, ao vencer os ateniences, condonou-os a enviar a cada ano, 14 adolescentes, 7 rapazes e 7 donzelas, que serviriam de banquete para o Minotauro. ",
         alternativas: [
             {
                 texto: "Isso é assustador!",
                 afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                    "No início você ficou com medo de enfrentá-lo, mas uma notícia mudou tudo, sua(seu) irmã(o) seria levado até o monstro, e você decide ajudá-lo(a).",
+                    
                 ],
                 proxima: 1,
             },
