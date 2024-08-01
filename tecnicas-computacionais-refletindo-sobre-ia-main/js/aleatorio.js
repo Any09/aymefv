d345b25c1937660274f89a3364cc4e1263962db0
Anny <104777994+Any09@users.noreply.github.com>
2024-08-01 13:27:42 -0300

    rtrt

--- tecnicas-computacionais-refletindo-sobre-ia-main/js/aleatorio.js
+++ tecnicas-computacionais-refletindo-sobre-ia-main/js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "", "Teseu"];
+const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "", "Ana"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
